1898735d1f6c3bee840dc98202cfe1ebecb19725
ArielAMR <arielamriou@gmail.com>
2024-03-27 23:51:14 +0100

    nearly finished

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//
+
 const switchThemeBtn = document.querySelector('.btn-toggle')
 let toggleTheme = true;
 
@@ -22,4 +24,20 @@ switchThemeBtn.addEventListener('click', () => {
 
         toggleTheme=!toggleTheme
     }
+})
+
+
+//question section
+
+const question1 = document.querySelector('.question1')
+const question2 = document.querySelector('.question2')
+
+
+
+question1.addEventListener('click', () => {
+    document.getElementById('awnser').innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ipsum porro quae fugit voluptatum hic, modi temporibus, et quo ipsa cumque eos autem ducimus nisi praesentium rerum tempore quia consequuntur laboriosam. Quia veritatis asperiores numquam ipsam sint iusto deleniti et atque hic temporibus, quisquam vero sequi omnis laborum consequuntur sit ullam expedita optio ratione animi esse cumque ipsa voluptatum! Ratione labore temporibus accusamus molestiae placeat praesentium. At magnam aliquid est aspernatur quam aliquam ducimus suscipit voluptas veritatis eaque. Qui dolore tempore eligendi, sunt eum expedita aut facere error quod explicabo repellendus odit nam accusamus debitis fugiat tenetur et aliquam amet!'
+})
+
+question2.addEventListener('click', () => {
+    document.getElementById('awnser').innerHTML='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit saepe, maiores ipsum quos officia quod. Dolore molestias, eius quis expedita illum ea? Non numquam necessitatibus quia porro suscipit voluptatum, vitae expedita, sequi dolore harum deserunt! Nemo adipisci numquam natus laboriosam dicta, est saepe earum placeat in magnam blanditiis libero eum fuga alias minima, voluptatem consequatur non ipsa nobis quis ex, veniam voluptate illo. Quia repellendus eaque accusamus doloremque, reprehenderit, omnis incidunt quae dolor dicta, sequi laudantium sit nemo atque minus!'
 })

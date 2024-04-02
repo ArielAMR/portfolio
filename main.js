690e38c5196847215bf8a0dafedc3dcd1d491814
ArielAMR <156708599+ArielAMR@users.noreply.github.com>
2024-04-02 13:22:37 +0200

    question fixed

--- main.js
+++ main.js
@@ -41,15 +41,15 @@ const question2 = document.querySelector('.question2')
 question1.addEventListener('click', () => {
     document.getElementById('awnser').innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ipsum porro quae fugit voluptatum hic, modi temporibus, et quo ipsa cumque eos autem ducimus nisi praesentium rerum tempore quia consequuntur laboriosam. Quia veritatis asperiores numquam ipsam sint iusto deleniti et atque hic temporibus, quisquam vero sequi omnis laborum consequuntur sit ullam expedita optio ratione animi esse cumque ipsa voluptatum! Ratione labore temporibus accusamus molestiae placeat praesentium. At magnam aliquid est aspernatur quam aliquam ducimus suscipit voluptas veritatis eaque. Qui dolore tempore eligendi, sunt eum expedita aut facere error quod explicabo repellendus odit nam accusamus debitis fugiat tenetur et aliquam amet!'
     document.getElementById('question').innerHTML='Question 1'
-    document.documentElement.style.setProperty('--border-1', 'solid')
-    document.documentElement.style.setProperty('--border-2', 'hidden')
+    document.documentElement.style.setProperty('--border-2', 'solid #FAF7FF')
+    document.documentElement.style.setProperty('--border-1', 'solid hsla(0, 0, 0, 0)')
 })
 
 question2.addEventListener('click', () => {
     document.getElementById('awnser').innerHTML='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit saepe, maiores ipsum quos officia quod. Dolore molestias, eius quis expedita illum ea? Non numquam necessitatibus quia porro suscipit voluptatum, vitae expedita, sequi dolore harum deserunt! Nemo adipisci numquam natus laboriosam dicta, est saepe earum placeat in magnam blanditiis libero eum fuga alias minima, voluptatem consequatur non ipsa nobis quis ex, veniam voluptate illo. Quia repellendus eaque accusamus doloremque, reprehenderit, omnis incidunt quae dolor dicta, sequi laudantium sit nemo atque minus!'
     document.getElementById('question').innerHTML='Question 2'
-    document.documentElement.style.setProperty('--border-1', 'hidden')
-    document.documentElement.style.setProperty('--border-2', 'solid')
+    document.documentElement.style.setProperty('--border-2', 'solid hsla(0, 0, 0, 0)')
+    document.documentElement.style.setProperty('--border-1', 'solid #FAF7FF')
 })
 
 //animations skills graph visible
@@ -93,9 +93,24 @@ window.addEventListener('mousemove', function (e) {
 
 //cursor hover links
 let links = document.querySelectorAll('a');
-
+let qN = document.querySelectorAll('.question');
 
 links.forEach(function(elem) {
+    elem.addEventListener('mouseenter', function () {
+        cursorOutline.animate({
+            width: '15px',
+            height: '15px'
+        }, { duration: 100, fill: 'forwards'});
+    }) 
+    elem.addEventListener('mouseleave', function () {
+        cursorOutline.animate({
+            width: '30px',
+            height: '30px'
+        }, { duration: 100, fill: 'forwards'});
+    }) 
+});
+
+qN.forEach(function(elem) {
     elem.addEventListener('mouseenter', function () {
         cursorOutline.animate({
             width: '15px',
